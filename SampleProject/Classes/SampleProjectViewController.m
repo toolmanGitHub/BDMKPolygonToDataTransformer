06711a404a8d20d752685963cf214d6988d31726
Firstname Lastname <timthetoolmantaylor@gmail.com>
2011-05-17 23:53:32 -0700

    First Import

---
+++ SampleProject/Classes/SampleProjectViewController.m
@@ -0,0 +1,224 @@
+//
+//  SampleProjectViewController.m
+//  SampleProject
+//
+//  Created by Tim Taylor on 5/17/11.
+//  Copyright 2011 Big Diggy SW. All rights reserved.
+//
+
+/*
+ 
+ The below license is the new BSD license with the OSI recommended personalizations.
+ <http://www.opensource.org/licenses/bsd-license.php>
+ 
+ Copyright (C) 2011 Big Diggy SW. All Rights Reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are
+ met:
+ 
+ * Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ * Neither the name of Tim Taylor nor Big Diggy SW 
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY Big Diggy SW "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ */
+
+
+#import "SampleProjectViewController.h"
+#import "BDMKPolygonToDataTransformer.h"
+@implementation SampleProjectViewController
+@synthesize myMapView;
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	[self moveFileToDocDirectory];
+	
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+	[super viewDidAppear:animated];
+	MKPolygon *thePolygon=[self polygonFromFileForName:@"WashingtonSample"];
+	
+	[self.myMapView addOverlay:thePolygon];
+	
+	[self.myMapView setVisibleMapRect:[thePolygon boundingMapRect] animated:YES];
+	
+}
+- (void)moveFileToDocDirectory{
+	//   //NSLog@"createEditableCopyOfDatabaseIfNeeded start");
+	BOOL success;
+	NSString *writablePath = [[self applicationDocumentDirectory] stringByAppendingPathComponent:@"WashingtonSample.bdl"];
+
+	// First, test for existence.
+	NSFileManager *fileManager = [NSFileManager defaultManager];
+	success = [fileManager fileExistsAtPath:writablePath];
+	if (success) return;
+	NSLog(@"creating a copy of the file");
+	
+	NSString *defaultPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"WashingtonSample.bdl"];
+	NSLog(@"defaultPath=%@",defaultPath);
+	NSError *fileCopyError=nil;
+	success = [fileManager copyItemAtPath:defaultPath toPath:writablePath error:&fileCopyError];
+	//	//NSLog@"  9");
+	if (!success) {
+		NSLog(@"couldn't create the writable file");
+		NSAssert1(0, @"Failed to create the file with message '%@'.", [fileCopyError localizedDescription]);
+	}
+	NSLog(@"file copied");
+	
+   	//	//NSLog@"createEditableCopyOfDatabaseIfNeeded end");
+}
+
+
+
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+
+-(MKPolygon *)polygonFromFileForName:(NSString *)fileName{
+	NSURL *dataURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.bdl",fileName]];
+	NSData *theData=[NSData dataWithContentsOfURL: dataURL];
+	BDMKPolygonToDataTransformer *myTransformer=[[BDMKPolygonToDataTransformer alloc] init];
+	
+	MKPolygon *thePolygon=[myTransformer reverseTransformedValue:theData];
+	
+	return thePolygon;
+	
+}
+
+-(void)writePolygonToFile:(MKPolygonView *)thePolygon withName:(NSString *)fileName{
+	NSURL *dataURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.bdl",fileName]];
+	
+	BDMKPolygonToDataTransformer *myTransformer=[[BDMKPolygonToDataTransformer alloc] init];
+	NSData *theData=[myTransformer transformedValue:thePolygon];
+	//	NSLog(@"the size of the compressed polygon:  %d",[theData length]);
+	
+	[theData writeToURL:dataURL atomically:YES];
+	[myTransformer release];
+}
+
+#pragma mark -
+#pragma mark Application's Documents directory
+
+/**
+ Returns the URL to the application's Documents directory.
+ */
+- (NSURL *)applicationDocumentsDirectory {
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
+- (NSString *)applicationDocumentDirectory {
+	return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+
+
+#pragma mark -
+#pragma mark Mapkit Deletage Methods
+
+
+// Mapkit
+
+// Monitoring Region Changes
+- (void)mapView:(MKMapView *)mapView regionWillChangeAnimated:(BOOL)animated{
+	//	NSLog(@"	mapView regionWillChangeAnimated");
+}
+
+- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated{
+	//	NSLog(@"	mapView regionDidChangeAnimated");
+	
+}
+
+// Loading the map
+- (void)mapViewDidFinishLoadingMap:(MKMapView *)mapView{
+	//	NSLog(@"	mapView mapViewDidFinishLoadingMap");
+}
+
+- (void)mapViewWillStartLoadingMap:(MKMapView *)mapView{
+	//	NSLog(@"	mapView mapViewWillStartLoadingMap");
+}
+
+- (void)mapViewDidFailLoadingMap:(MKMapView *)mapView withError:(NSError *)error{
+	//	NSLog(@"	mapView mapViewDidFailLoadingMap Error:  %@",[error userInfo]);
+}
+
+
+
+// Locating the User
+- (void)mapViewWillStartLocatingUser:(MKMapView *)mapView{
+	//	NSLog(@"mapView mapViewWillStartLocatingUser");
+}
+- (void)mapViewDidStopLocatingUser:(MKMapView *)mapView{
+	//	NSLog(@"mapView mapViewDidStopLocatingUser");
+	
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
+	//	NSLog(@"mapView didUpdateUserLocation:  %@",[userLocation description]);
+	//	self.myMapView.showsUserLocation=NO;
+}
+
+- (MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id <MKOverlay>)overlay{
+	
+	MKPolygonView *theView=[[MKPolygonView alloc] initWithOverlay:overlay];
+	theView.fillColor=[UIColor blueColor];
+	theView.strokeColor=[UIColor blackColor];
+
+	theView.alpha=0.35;
+	
+	//NSLog(@"MultiPolygonView=%@",theView);
+	return [theView autorelease];;
+}
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation{
+	MKPinAnnotationView *pin =	[[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
+	pin.canShowCallout = YES;
+	pin.animatesDrop = YES;
+	return [pin autorelease];
+}
+
+#pragma mark -
+#pragma mark Memory Management
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+	self.myMapView=nil;
+}
+
+
+- (void)dealloc {
+	[myMapView release];
+	
+    [super dealloc];
+}
+
+@end
